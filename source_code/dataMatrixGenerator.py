1f608454d8cf9cdbe831568a41be507ca5330605
Quan Bach <qbach@scu.edu>
2020-12-04 15:35:48 -0800

    add tf-idf script to generate weight matrices.

---
+++ source_code/dataMatrixGenerator.py
@@ -0,0 +1,56 @@
+"""
+Santa Clara University
+
+COEN 240 - Machine Learning
+
+Final Project
+
+Quan Bach
+Anh Truong
+
+By running this file, the weight matrix will be contructed based on TF-IDF algorithm.
+NOTICE: due to the hardware constraints the documents are divided into batch of 5000 for processing.
+
+*** ATTENTION *** this program is not optimized. Therefore, running this script might take more than 20 mininutes - depends on the hardware. Please refer to the TF-IDF folder for the generated matrices. If folder does not exist, this script must be ran.
+
+Author hardware configuration:
+    OS:         macOS Catalina
+    Processor:  2.5Ghz Quad-Core Intel Cire i7
+    Memory:     16 GB 1600 MHz DDR3
+    
+"""
+
+import math
+import os 
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+
+if __name__ == "__main__":
+    
+    if not os.path.exists('./tf-idf/'):
+            os.makedirs('./tf-idf/')
+    
+    content_list = np.load('./npy/content_list.npy', allow_pickle=True)
+
+
+    partition = math.ceil(len(content_list)/5000)
+    vectorizer = TfidfVectorizer() # initialize vectorizer for the algorithm
+
+    # partiioning the corprus into batch of max size 5000 each
+    for idx in range(0,partition):
+        if idx == partition - 1:
+            mylist = content_list[idx*5000:]
+        else:
+            mylist = content_list[idx*5000 : (idx + 1)*5000]
+            
+        vectors = vectorizer.fit_transform(mylist)
+        feature_names = vectorizer.get_feature_names()
+        dense = vectors.todense()
+        denselist = dense.tolist()
+        df = pd.DataFrame(denselist, columns=feature_names)
+        compression_opts = dict(method='zip',archive_name='test.csv')
+        df.to_csv(('./tf-idf/tf-idf' + str(idx+1) + '.zip'),index = False, compression=compression_opts)
+    
+  
